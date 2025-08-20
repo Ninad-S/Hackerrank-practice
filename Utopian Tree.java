f4e0791e2ef1b375f60179913abb474a44a56258
Ninad-S <144244166+Ninad-S@users.noreply.github.com>
2025-08-20 13:50:20 -0500

    Create Utopian Tree.java

---
+++ Utopian Tree.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'utopianTree' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+    public static int utopianTree(int n,int i,int height) {
+    // Write your code here
+        if (i>n)
+        {
+            return height;
+        }
+        if (i%2==1)
+        {
+            height *=2;
+        }
+        else
+        {
+            height +=1;
+        }
+        return utopianTree(n,i+1,height);
+    }
+    public static int utopianTree(int n) {
+    // Write your code here
+        return utopianTree(n,1,1);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int result = Result.utopianTree(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
